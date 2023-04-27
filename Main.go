136fc99458ba6058965e12ba17eada20255f402d
Rafael Morazan <rafaelmorazan09@gmail.com>
2023-04-27 15:05:13 -0600

    declaring variables

--- Main.go
+++ Main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+var l int = 45
+
+var (
+	pokemonName   string = "Pikachu"
+	pokemonNumber int    = 25
+	pokemonRegion string = "Kanto"
+)
+
 func main() {
-	fmt.Println("Hello Go!")
+	var i int
+	i = 42
+	var j int = 27
+	k := 32
+	fmt.Println(i, j, k, l)
+	fmt.Println(pokemonName, pokemonNumber, pokemonRegion)
 }
